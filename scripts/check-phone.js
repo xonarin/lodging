996918337a71dfe4ff04ff30a3436596427a0542
Andrey Arkhipov <arhipov@yandex.ru>
2021-12-05 18:24:04 +0300

    Доавлено повление и исчезнование ошибки

--- scripts/check-phone.js
+++ scripts/check-phone.js
@@ -1,12 +1,17 @@
 const phoneInput = document.querySelector("#tel");
+let errorContainer;
 
 const regExpPhone =
   /(^\+7|8{1})[\s\+-:\(\)]*(\d{3})[\s\+-:\(\)]*(\d{3})[\s\+-:\(\)]*(\d{2})[\s\+-:\(\)]*(\d{2})$/;
 
+let errorFlag = false;
 phoneInput.addEventListener("change", (event) => {
   if (phoneInput.value.match(regExpPhone)) {
     phoneInput.value = phoneInput.value.replace(regExpPhone, "+7($2)$3-$4-$5");
-  } else {
+    phoneInput.style = undefined;
+    errorContainer.style.display = "none";
+    errorFlag = false;
+  } else if (!errorFlag) {
     phoneInput.style.borderColor = "#f40934";
     phoneInput.style.marginBottom = "0";
     const errorElement = errorTemplate
@@ -17,6 +22,8 @@ phoneInput.addEventListener("change", (event) => {
     errorElement.querySelector(".form__error-text").textContent =
       "пожалуйста, введите номер телефона в формате +7(xxx)xxx-xx-xx";
     phoneInput.after(errorElement);
+    errorContainer = document.querySelector(".form__error-container");
+    errorFlag = true;
   }
 });
 

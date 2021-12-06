005a47d96f57370f3bf7634b54b5852e54122030
Andrey Arkhipov <arhipov@yandex.ru>
2021-12-06 17:03:18 +0300

    1. Удаление сообщений об ошибках при правильном вводе. 2. рефакторинг форм-эрорс 3. интеграция фон-чек в форм-эрорс 4. улучшеная валидация телефона 5. Исправление дублей ошибок 6. Заменил inputs.querySelector с input на .form__input

--- scripts/form__error.js
+++ scripts/form__error.js
@@ -1,17 +1,72 @@
-const inputs = document.querySelectorAll('input');
-const errorTemplate = document.querySelector('#form__error').content;
+const inputs = document.querySelectorAll(".form__input");
+const castomeSelect = document.querySelector("#city");
+const errorTemplate = document.querySelector("#form__error").content;
+const phoneInput = document.querySelector("input[type=tel]");
+const regExpPhone =
+  /(^\+7|8{1})[\s\+-:\(\)]*(\d{3})[\s\+-:\(\)]*(\d{3})[\s\+-:\(\)]*(\d{2})[\s\+-:\(\)]*(\d{2})$/;
+const regExpDigits = /[A-Za-zА-Яа-яЁё]/;
+let errorImg;
+let errorMsg;
+let errorFlag = false;
 
-Array.from(inputs).forEach(input => {
+function createErrorMsg(element, textError) {
+  if (!element.style.marginBottom) {
+    element.style.borderColor = "#f40934";
+    element.style.marginBottom = "0";
+    const errorElement = errorTemplate
+      .querySelector(".form__error-container")
+      .cloneNode(true);
+    errorImg = errorElement.querySelector(".form__error-img").src =
+      "./images/form-error.svg";
+    errorMsg = errorElement.querySelector(".form__error-text").textContent =
+      textError;
+    element.after(errorElement);
+  }
+}
 
-	input.addEventListener('invalid', function (event) {
-		event.preventDefault();
-		if (!event.target.validity.valid) {
-			input.style.borderColor = '#f40934';
-			input.style.marginBottom = '0';
-			const errorElement = errorTemplate.querySelector('.form__error-container').cloneNode(true);
-			errorElement.querySelector('.form__error-img').src = './images/form-error.svg';
-			errorElement.querySelector('.form__error-text').textContent = 'пожалуйста, заполните это поле'
-			input.after(errorElement);
-		}
-	})
+function deleteError(element) {
+  if (errorMsg && errorImg) {
+    element.style.borderColor = "";
+    element.style.marginBottom = "";
+    element.nextSibling.remove();
+  }
+}
+
+inputs.forEach((input) => {
+  input.addEventListener("invalid", (event) => {
+    if (!event.target.validity.valid) {
+      createErrorMsg(input, "пожалуйста, заполните это поле");
+    }
+  });
+  input.addEventListener("change", (event) => {
+    if (event.target.validity.valid) {
+      deleteError(input);
+    }
+  });
+});
+
+castomeSelect.addEventListener("blur", () => {
+  deleteError(castomeSelect);
+});
+
+phoneInput.addEventListener("change", (event) => {
+  if (phoneInput.value.match(regExpDigits)) {
+    phoneInput.value = "+7(";
+  } else if (!phoneInput.value.match(regExpPhone)) {
+    if (!errorFlag) {
+      deleteError(phoneInput);
+      createErrorMsg(
+        phoneInput,
+        "введите номер телефона в формате +7(xxx)xxx-xx-xx"
+      );
+      errorFlag = true;
+    }
+  } else {
+    phoneInput.value = phoneInput.value.replace(regExpPhone, "+7($2)$3-$4-$5");
+    phoneInput.style = undefined;
+    deleteError(phoneInput);
+    errorFlag = false;
+    phoneInput.removeAttribute("minLength");
+    phoneInput.removeAttribute("pattern");
+  }
 });
